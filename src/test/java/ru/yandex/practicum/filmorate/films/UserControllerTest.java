ac1d80b34ed5e37458c4ff0601c0c76a45bbdcd8
vlolad <vlad19.99@yandex.ru>
2022-08-31 00:47:33 +0300

    update tests

--- src/test/java/ru/yandex/practicum/filmorate/films/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/films/UserControllerTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.controller.UserController;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.UserService;
 import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
+import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
@@ -32,7 +34,8 @@ public class UserControllerTest {
 
     @BeforeEach
     public void createController() {
-        controller = new UserController(new InMemoryUserStorage());
+        UserStorage storage = new InMemoryUserStorage();
+        controller = new UserController(storage, new UserService(storage));
     }
 
     @Test
